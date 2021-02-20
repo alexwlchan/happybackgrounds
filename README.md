be0500f4c06353505fa277acb878256068bb4625
Alex Chan <alex@alexwlchan.net>
2021-02-20 23:28:16 +0000

    Add some example backgrounds

---
+++ README.md
@@ -0,0 +1,7 @@
+<img src="snowflake.svg">
+
+<img src="starfield.svg">
+
+<img src="mars.svg">
+
+<img src="plants.svg">
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# python3 happybackgrounds.py \
+#     --background="#d6fcff" \
+#     --icon_name="snowflake" \
+#     --out_path="snowflake.svg"
+
+# python3 happybackgrounds.py \
+#     --background="#000314" \
+#     --icon_name="star" \
+#     --out_path="starfield.svg" \
+#     --min_scale=0.2 \
+#     --max_scale=0.6
+
+# python3 happybackgrounds.py \
+#     --background="#c24400" \
+#     --icon_name="rocket" \
+#     --out_path="mars.svg" \
+#     --min_scale=0.2 \
+#     --max_scale=0.6
+
+python3 happybackgrounds.py \
+    --background="#a7ff99" \
+    --icon_name="seedling" \
+    --out_path="plants.svg" \
+    --min_icon_count=35 \
+    --max_icon_count=40 \
+    --min_scale=0.1 \
+    --max_scale=0.25
